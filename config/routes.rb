e4f2b2b298a4a2402115fcdc067a20059b868c7c
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-16 10:51:45 +0200

    rails2+

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # This the syntax for making a home page (we prefer this because it jumps to the eye quickly)
+  # root to: 'game#home'
+  # same as:
+  # get '/', to: 'games#home'
+
   # verb "url", to: "controller#action"
   get "/new", to: "games#new"
   post "/score", to: "games#score"
